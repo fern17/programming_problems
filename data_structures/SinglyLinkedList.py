7eeba9c299121278f67f309c24064e6d679e3ec0
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-10-16 20:43:01 +0200

    add more useful methods and tests for our SinglyLinkedList

--- data_structures/SinglyLinkedList.py
+++ data_structures/SinglyLinkedList.py
@@ -40,6 +40,28 @@ class SinglyLinkedList:
             p = p.next
         return l
 
+    def front(self):
+        if self.head:
+            return self.head.value
+        else:
+            return None
+
+    def back(self):
+        if not self.head:
+            return None
+        p = self.head
+        while p.next:
+            p = p.next
+        return p.value
+
+    def push_front(self, v):
+        new_node = self.Node(v)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+
     def push_back(self, v):
         new_node = self.Node(v)
         if self.head:
@@ -67,6 +89,18 @@ class SinglyLinkedList:
         p.next = None
         return v
 
+    def pop_front(self):
+        if not self.head:
+            return None
+        if self.head.next:
+            v = self.head.value
+            self.head = self.head.next
+            return v
+        else:
+            v = self.head.value
+            self.head = None
+            return v
+
     def remove_node(self, n):
         if not self.head or not n:
             return None
@@ -84,24 +118,35 @@ class SinglyLinkedList:
 
 
 class Test(unittest.TestCase):
-    def test(self):
+    def test_basic(self):
         linked_list = SinglyLinkedList()
         self.assertEqual(True, linked_list.is_empty())
+        self.assertEqual(None, linked_list.back())
+        self.assertEqual(None, linked_list.front())
         self.assertEqual(0, linked_list.count())
         self.assertEqual([], linked_list.as_list())
         linked_list.push_back(1)
         self.assertEqual(1, linked_list.count())
         self.assertEqual([1], linked_list.as_list())
         self.assertEqual(False, linked_list.is_empty())
+        self.assertEqual(1, linked_list.back())
+        self.assertEqual(1, linked_list.front())
         linked_list.push_back(2)
         self.assertEqual([1, 2], linked_list.as_list())
+        self.assertEqual(1, linked_list.front())
+        self.assertEqual(2, linked_list.back())
         self.assertEqual(2, linked_list.count())
         self.assertEqual(2, linked_list.pop_back())
+        self.assertEqual(1, linked_list.front())
+        self.assertEqual(1, linked_list.back())
         self.assertEqual(1, linked_list.count())
         self.assertEqual(1, linked_list.pop_back())
         self.assertEqual(0, linked_list.count())
+        self.assertEqual(None, linked_list.back())
+        self.assertEqual(None, linked_list.front())
         self.assertEqual(True, linked_list.is_empty())
 
+    def test_equality(self):
         l1 = SinglyLinkedList()
         l2 = SinglyLinkedList()
 
@@ -118,11 +163,45 @@ class Test(unittest.TestCase):
 
         self.assertEqual(SinglyLinkedList([1, 2]), l2)
 
+    def test_removal(self):
         ll = SinglyLinkedList([1, 2, 3])
         n = ll.head
         m = n.next
         q = ll.remove_node(n)
         self.assertEqual(SinglyLinkedList([2, 3]), ll)
+
+    def test_push(self):
+        linked_list = SinglyLinkedList()
+        self.assertEqual(True, linked_list.is_empty())
+        self.assertEqual(0, linked_list.count())
+        linked_list.push_front(1)
+        self.assertEqual([1], linked_list.as_list())
+        self.assertEqual(1, linked_list.count())
+        linked_list.push_front(2)
+        self.assertEqual(2, linked_list.count())
+        self.assertEqual([2, 1], linked_list.as_list())
+        linked_list.push_back(3)
+        self.assertEqual([2, 1, 3], linked_list.as_list())
+
+    def test_pop(self):
+        linked_list = SinglyLinkedList()
+        self.assertEqual(None, linked_list.pop_front())
+        self.assertEqual(None, linked_list.pop_back())
+        linked_list.push_back(1)
+        self.assertEqual([1], linked_list.as_list())
+        self.assertEqual(1, linked_list.pop_front())
+        linked_list.push_back(2)
+        self.assertEqual(2, linked_list.pop_front())
+        linked_list.push_back(3)
+        linked_list.push_back(4)
+        self.assertEqual([3, 4], linked_list.as_list())
+        self.assertEqual(4, linked_list.pop_back())
+        self.assertEqual(3, linked_list.pop_back())
+        linked_list.push_back(5)
+        linked_list.push_back(6)
+        self.assertEqual(5, linked_list.pop_front())
+        self.assertEqual(6, linked_list.pop_front())
+        self.assertEqual(None, linked_list.pop_front())
 
 
 if __name__ == '__main__':
